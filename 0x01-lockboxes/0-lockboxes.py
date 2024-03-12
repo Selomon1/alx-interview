6d2067026bb94ef6dab96de359b86c2a2e70a153
Selomon1 <soya.vandy1950@gmail.com>
2024-03-12 20:19:41 +0300

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Lockboxes module """
+
+
+def canUnlockAll(boxes):
+    """
+    checking if available boxes can be opened
+    """
+
+    if not boxes:
+        return False
+
+    # Initialize a set to track the keys
+    keys = set([0])
+
+    # Initialize a set to track boxes can be accessed
+    accessible_boxes = set([0])
+
+    # Loop until any more boxes can't be accessed
+    while accessible_boxes:
+        box = accessible_boxes.pop()
+        for key in boxes[box]:
+            if key not in keys:
+                keys.add(key)
+                accessible_boxes.add(key)
+
+    return len(keys) == len(boxes)
