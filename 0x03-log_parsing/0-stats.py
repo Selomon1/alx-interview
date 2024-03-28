788fedc5f00423b6e2c07c70c5adac00eeacd43f
Selomon1 <soya.vandy1950@gmail.com>
2024-03-28 22:11:56 +0300

    Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,37 +4,37 @@ Script to compute metrics from log data read from stdin.
 """
 
 import sys
-import signal
 import re
 
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 
-def signal_handler(sig, frame):
-    """ Signal handler for keyboard inturruption """
-    print_stats(total_size, status_codes)
+total_size = 0
+line_count = 0
 
 
-def print_stats(total_size, status_codes):
+def print_stats():
     """
     Prints the total file size and number of lines by status code
     """
     print("File size: {}".format(total_size))
     for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
+        if status_codes[code]:
             print("{}: {}".format(code, status_codes[code]))
-
-
-signal.signal(signal.SIGINT, signal_handler)
-
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
 
 
 def parse_line(line):
     """ Parse a log entry line and extract IP, status_code """
-    match = re.match(r"^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[.*\] \"GET /projects/260 HTTP/1.1\" (\d{3}) (\d+)$", line)
+    match = re.match(r'^.*"GET /projects/260 HTTP/1\.1" (\d{3}) (\d+)$', line)
     if match:
-        return int(match.group(3)), int(match.group(4))
+        return int(match.group(1)), int(match.group(2))
     else:
         return None, None
 
@@ -45,13 +45,14 @@ try:
         if code is None:
             continue
         total_size += size
-        status_codes[code] += 1
-        line_count += 1
+        if code is not None and code in status_codes:
+            status_codes[code] += 1
+            line_count += 1
         if line_count == 10:
-            print_stats(total_size, status_codes)
+            print_stats()
             line_count = 0
 except KeyboardInterrupt:
     pass
 
 
-print_stats(total_size, status_codes)
+print_stats()
