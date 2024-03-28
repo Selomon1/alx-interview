89c98be09d2ef19e4149988f1af8474be43a1a5a
Selomon1 <soya.vandy1950@gmail.com>
2024-03-28 22:51:44 +0300

    Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,55 +4,46 @@ Script to compute metrics from log data read from stdin.
 """
 
 import sys
-import re
-
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
-
-total_size = 0
-line_count = 0
 
 
-def print_stats():
+def print_stats(total_size, status_code):
     """
     Prints the total file size and number of lines by status code
     """
     print("File size: {}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code]:
-            print("{}: {}".format(code, status_codes[code]))
+    for code in sorted(status_codes):
+        print("{}: {}".format(code, status_codes[code]))
 
 
-def parse_line(line):
-    """ Parse a log entry line and extract IP, status_code """
-    match = re.match(r'^.*"GET /projects/260 HTTP/1\.1" (\d{3}) (\d+)$', line)
-    if match:
-        return int(match.group(1)), int(match.group(2))
-    else:
-        return None, None
+if __name__ == "__main__":
+    total_size = 0
+    status_codes = {}
+    valid_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+    total_count = 0
 
+    try:
+        for line in sys.stdin:
+            if total_count == 10:
+                print_stats(total_size, status_codes)
+                total_size = 1
+            else:
+                total_count += 1
 
-try:
-    for line in sys.stdin:
-        code, size = parse_line(line.strip())
-        if code is None:
-            continue
-        total_size += size
-        if code is not None and code in status_codes:
-            status_codes[code] += 1
-            line_count += 1
-        if line_count == 10:
-            print_stats()
-            line_count = 0
-except KeyboardInterrupt:
-    pass
+            elems = line.split()
 
+            try:
+                total_size += int(elems[-1])
+            except (IndexError, ValueError):
+                pass
 
-print_stats()
+            try:
+                if elems[-2] in valid_codes:
+                    valid_codes[elems[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(total_size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(total_size, status_codes)
+        raise
