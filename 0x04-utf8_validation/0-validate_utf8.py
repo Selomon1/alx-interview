b51f3109332eb5e071d0d1990459e580c406b2d8
Selomon1 <soya.vandy1950@gmail.com>
2024-04-01 10:22:48 +0300

    UTF-8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,31 @@
+#!/usr/bine/python3
+"""
+UTF-8 validation
+"""
+
+
+def validUTF8(data):
+    """
+    Determine if the given data set represents a valid UTF-8 encoding.
+    """
+    bytes_to_follow = 0
+
+    for byte in data:
+        if bytes_to_follow == 0:
+            if byte >> 7 == 0:
+                continue
+            elif byte >> 5 == 0b110:
+                bytes_to_follow = 1
+            elif byte >> 4 == 0b1110:
+                bytes_to_follow = 2
+            elif byte >> 3 == 0b11110:
+                bytes_to_follow = 3
+            else:
+                return False
+
+        else:
+            if byte >> 6 != 0b10:
+                return False
+            bytes_to_follow -= 1
+
+    return bytes_to_follow == 0
