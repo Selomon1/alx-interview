644aefe230349e6e5234ee2fd40e3a0f7e84f39f
Selomon1 <soya.vandy1950@gmail.com>
2024-04-02 19:15:08 +0300

    Validate UTF-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,7 +12,7 @@ def validUTF8(data):
 
     for byte in data:
         if bytes_to_follow == 0:
-            if byte >> 7 == 0b1:
+            if byte >> 7 == 0:
                 continue
             elif byte >> 5 == 0b110 or byte >> 5 == 0b1110:
                 bytes_to_follow = 1
@@ -20,6 +20,8 @@ def validUTF8(data):
                 bytes_to_follow = 2
             elif byte >> 3 == 0b11110:
                 bytes_to_follow = 3
+            else:
+                return False
 
         else:
             if (byte >> 6) != 0b10:
