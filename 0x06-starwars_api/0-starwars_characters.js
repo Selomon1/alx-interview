65abea90732497c31b87964ffc90e607b8ab4ea4
Selomon1 <soya.vandy1950@gmail.com>
2024-04-21 03:53:21 +0300

    StarWars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,35 +5,35 @@ const movieId = process.argv[2];
 const url = `https://swapi.dev/api/films/${movieId}/`;
 
 request(url, (error, response, body) => {
-	if (error) {
-		console.error('Error:', error);
-	} else if (response.statusCode !== 200) {
-		console.error('Status:', response.statusCode);
-	} else {
-		const film = JSON.parse(body)
-		const characterUrls = film.characters;
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Status:', response.statusCode);
+  } else {
+    const film = JSON.parse(body);
+    const characterUrls = film.characters;
 
-		const promises = characterUrls.map(characterUrl => {
-			return new Promise((resolve, reject) => {
-				request(characterUrl, (error, response, body) => {
-					if (error) {
-						reject(error);
-					} else if (response.statusCode !== 200) {
-						reject(`Failed to fetch character data for ${chracterUrl}`);
-					} else {
-						const character = JSON.parse(body);
-						resolve(character.name);
-					}
-				});
-			});
-		});
+    const promises = characterUrls.map(characterUrl => {
+      return new Promise((resolve, reject) => {
+        request(characterUrl, (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else if (response.statusCode !== 200) {
+            reject(new Error(`Failed to fetch character data for ${characterUrl}`));
+          } else {
+            const character = JSON.parse(body);
+            resolve(character.name);
+          }
+        });
+      });
+    });
 
-		Promise.all(promises)
-			.then(characterNames => {
-				characterNames.forEach(name => console.log(name));
-			})
-			.catch(error => {
-				console.error('Error:', error);
-			});
-	}
+    Promise.all(promises)
+      .then(characterNames => {
+        characterNames.forEach(name => console.log(name));
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
 });
