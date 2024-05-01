293215b574ef99419cd1dc2e3a0221965dbf1d04
Selomon1 <soya.vandy1950@gmail.com>
2024-05-01 21:21:52 +0300

    Making Change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for making change with the fewest number of coins."""
+
+
+def makeChange(coins, total):
+    """Calculate the fewest number of coins needed to meet a given."""
+    if total < 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
