3faaa985e38db2c6991976f4f6993b7272cc19cf
Selomon1 <soya.vandy1950@gmail.com>
2024-05-02 08:51:43 +0300

    Making Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,13 +7,13 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    coins.sort()
 
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
     for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+        if coin <= total:
+            for amount in range(coin, total + 1):
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
